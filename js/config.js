f94f085534c38e82c0d2e42e794da9741b38c399
Nasser Ali Karimi <nasseralikarimi@gmail.com>
2025-05-08 20:51:35 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_POEHLLo3K', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '52nt7o975sjco3la1beklics2v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
